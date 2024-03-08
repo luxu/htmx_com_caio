3a9960de981d6507e8814287a23f4b0180df6ba4
Luciano Martins <zicadopv@gmail.com>
2024-03-08 13:47:28 -0300

    atualizando .gitignore settings

--- htmx/settings.py
+++ htmx/settings.py
@@ -27,7 +27,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django_htmx.middleware.HtmxMiddleware',
 ]
 
 ROOT_URLCONF = 'htmx.urls'
